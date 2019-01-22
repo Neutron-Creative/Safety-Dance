e9bd22371607e15deef60f3cd8744b40a74e51b1
Jimmy Bisenius <me@jimmybweb.com>
2019-01-22 03:15:50 -0500

    Added additional debug pt 2

--- config/defaults.php
+++ config/defaults.php
@@ -290,7 +290,7 @@ $core_hooks = [
 			Hooks::TAG      => 'genesis_before_loop',
 			Hooks::CALLBACK => function () {
 				if(is_product_category()) {
-					echo '<h1>' . get_the_title() . ' yeee</h1>';
+					echo '<h1>is a catergory</h1>';
 				} else {
 					echo '<h1>not category</h1>';
 				}
