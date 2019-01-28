dec4c83ff57f4b34c390f7998dd7aee510fdbebb
Jimmy Bisenius <me@jimmybweb.com>
2019-01-28 04:00:38 -0500

    Added new badge to products

--- config/defaults.php
+++ config/defaults.php
@@ -404,6 +404,9 @@ $core_hooks = [
 								}
 								echo "<div style='display:block;width:100%;height:2px;margin-bottom:10px;margin-top:0;background-color:#0094DE;'></div>";
 								echo "<div class='title-row'>";
+									if(in_array('new', $specificationBadges)) {
+										echo '<img src="/wp-content/uploads/2019/01/new.gi"/>';
+									}
 									echo "<h3>" . $product->get_name() . "</h3>";
 									if(in_array('millenium', $specificationBadges)) {
 										echo '<img src="/wp-content/uploads/2019/01/millenium.gif"/>';
